3b25ec3da4bcd10191689384f5c1784af872fbff
Bartłomiej Makarewicz <bartlomiej.makarewicz@gmail.com>
2018-09-15 11:32:39 +0200

    Initial Spring Boot project

---
+++ src/main/java/nand/revoluttest/RevolutTestApplication.java
@@ -0,0 +1,12 @@
+package nand.revoluttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevolutTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RevolutTestApplication.class, args);
+  }
+}
